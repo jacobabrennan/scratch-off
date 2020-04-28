920933ccb23de729d0cec3352f511bc8bf455173
Jacob A Brennan <jacobabrennan@gmail.com>
2020-04-28 10:43:32 -0400

    Add ability to scratch; draw scratched layer

--- scratcher.js
+++ scratcher.js
@@ -18,11 +18,14 @@
 //-- Dependencies --------------------------------
 import Vue from './vue.esm.browser.js';
 
+//-- Constants -----------------------------------
+const SCRATCH_LINE_WIDTH = 32;
+
 //------------------------------------------------
 Vue.component('image-scratcher', {
     template: (`
         <keep-alive>
-            <canvas style="border: double 3px black" />
+            <canvas @mousemove="handleMouseMove" style="border: double 3px black" />
         </keep-alive>
     `),
     data: function () {
@@ -31,6 +34,8 @@ Vue.component('image-scratcher', {
             imageBackground: new Image(),
             foregroundReady: false,
             backgroundReady: false,
+            lastMoveX: null,
+            lastMoveY: null,
         };
     },
     props: {
@@ -52,6 +57,10 @@ Vue.component('image-scratcher', {
         },
     },
     created() {
+        //
+        let scratchCanvas = document.createElement('canvas');
+        this.scratchContext = scratchCanvas.getContext('2d');
+        //
         this.imageForeground.addEventListener('load', () => {
             this.foregroundReady = true;
         });
@@ -85,17 +94,26 @@ Vue.component('image-scratcher', {
     },
     methods: {
         handleSizeSet() {
+            //
+            const scratchCanvas = this.scratchContext.canvas;
+            scratchCanvas.width = this.width;
+            scratchCanvas.height = this.height;
+            //
             this.$el.width = this.width;
             this.$el.height = this.height;
             this.draw();
         },
         draw() {
             if(!this.foregroundReady || !this.backgroundReady) { return;}
-            // Draw top layer
+            // Draw top layer with scratched portion removed
             this.context.save();
             this.context.fillStyle = 'black';
             this.context.fillRect(0, 0, this.width, this.height);
-            this.centerImage(this.foreground);
+            this.context.globalCompositeOperation = 'destination-out';
+            this.context.drawImage(this.scratchContext.canvas, 0, 0);
+            // Draw foreground onto scratch layer content
+            this.context.globalCompositeOperation = 'source-atop';
+            this.centerImage(this.imageForeground);
             // Fill background in empty area, and crop to background shape
             this.context.globalCompositeOperation = 'destination-atop';
             this.centerImage(this.imageBackground);
@@ -118,5 +136,34 @@ Vue.component('image-scratcher', {
             }
             this.context.drawImage(image, offsetX, offsetY, drawWidth, drawHeight);
         },
+        handleMouseMove(mouseEvent) {
+            // Calculate coordinates of event relative to the canvas
+            const bounds = this.$el.getBoundingClientRect();
+            const moveEndX = mouseEvent.clientX - bounds.left;
+            const moveEndY = mouseEvent.clientY - bounds.top;
+            // Compare to previous events (or initialize if first)
+            let moveStartX = this.lastMoveX;
+            let moveStartY = this.lastMoveY;
+            if(moveStartX === null) { moveStartX = moveEndX;}
+            if(moveStartY === null) { moveStartY = moveEndY;}
+            // Store last movement on state, for future comparisons
+            this.lastMoveX = moveEndX;
+            this.lastMoveY = moveEndY;
+            // Scratch line from previous coordinates to current coordinates
+            this.eraseScratchLine(moveStartX, moveStartY, moveEndX, moveEndY);
+            // Check if canvas is completely scratched
+            // Redraw
+            this.draw();
+        },
+        eraseScratchLine(startX, startY, endX, endY) {
+            // Draw a line on compositing canvas from start(x,y) to end(x,y)
+            this.scratchContext.strokeStyle = 'black';
+            this.scratchContext.lineWidth = SCRATCH_LINE_WIDTH;
+            this.scratchContext.beginPath();
+            this.scratchContext.moveTo(startX, startY);
+            this.scratchContext.lineTo(endX, endY);
+            this.scratchContext.closePath();
+            this.scratchContext.stroke();
+        }
     },
 });
