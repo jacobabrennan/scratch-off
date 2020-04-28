f744e66b4c4a8e8bbea3e6a1a8a1a56517506e38
Jacob A Brennan <jacobabrennan@gmail.com>
2020-04-28 08:45:04 -0400

    Pass foreground and background images as urls

--- scratcher.js
+++ scratcher.js
@@ -25,6 +25,12 @@ Vue.component('image-scratcher', {
             <canvas style="border: double 3px black" />
         </keep-alive>
     `),
+    data: function () {
+        return {
+            imageForeground: new Image(),
+            imageBackground: new Image(),
+        };
+    },
     props: {
         width: {
             type: Number,
@@ -34,13 +40,41 @@ Vue.component('image-scratcher', {
             type: Number,
             required: true,
         },
+        foreground: {
+            type: String,
+            required: true,
+        },
+        background: {
+            type: String,
+            required: true,
+        },
+    },
+    created() {
+        this.imageForeground.addEventListener('load', () => {
+            this.foregroundReady = true;
+        });
+        this.imageBackground.addEventListener('load', () => {
+            this.backgroundReady = true;
+        });
     },
     mounted() {
         this.handleSizeSet();
     },
     watch: {
-        width: 'handleSizeSet',
-        height: 'handleSizeSet',
+        foreground: {
+            handler: function (valueNew) {
+                this.foregroundReady = false;
+                this.imageForeground.src = valueNew;
+            },
+        },
+        background: {
+            handler: function (valueNew) {
+                this.backgroundReady = false;
+                this.imageBackground.src = valueNew;
+            },
+        },
+        foregroundReady: 'draw',
+        backgroundReady: 'draw',
     },
     methods: {
         handleSizeSet() {
